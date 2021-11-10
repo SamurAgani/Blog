cbde79062437b340c7dc77fa1ec1b7a8aca8ac49
SamurAgani <76554296+SamurAgani@users.noreply.github.com>
2021-11-10 23:30:57 +0400

    new

---
+++ EntityLayer/Concrete/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Contact
+    {
+        [Key]
+        public int ContentID { get; set; }
+        public string ContentUserName { get; set; }
+        public string ContentMail { get; set; }
+        public string ContentSubject { get; set; }
+        public string ContentMessage { get; set; }
+        public DateTime ContentDate { get; set; }
+        public bool ContentStatus { get; set; }
+    }
+}
