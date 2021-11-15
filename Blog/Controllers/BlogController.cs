39d3ff17c8659f24495c0085b8c0e410e091c41f
SamurAgani <76554296+SamurAgani@users.noreply.github.com>
2021-11-15 13:23:26 +0400

    ViewContinue

--- Blog/Controllers/BlogController.cs
+++ Blog/Controllers/BlogController.cs
@@ -1,4 +1,6 @@
 using Blog.Models;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -10,10 +12,17 @@ using System.Threading.Tasks;
 namespace Blog.Controllers
 {
     public class BlogController : Controller
-    {        
+    {
+        BlogManeger bm = new BlogManeger(new EfBlogRepository());
         public IActionResult Index()
         {
-            return View();
+            List<EntityLayer.Concrete.Blog> blogs = new List<EntityLayer.Concrete.Blog>();
+            blogs.Add(new EntityLayer.Concrete.Blog() { BlogTitle="C# ile asinxron", BlogImage = "/web/images/7.jpg",  BlogCreateDate = DateTime.Now, BlogStatus = true, CategoryID = 4 });
+            blogs.Add(new EntityLayer.Concrete.Blog() { BlogTitle="Phyton ile asinxron", BlogImage = "/web/images/7.jpg",  BlogCreateDate = DateTime.Now, BlogStatus = true, CategoryID = 1 });
+            blogs.Add(new EntityLayer.Concrete.Blog() { BlogTitle="In to the wild", BlogImage = "/web/images/7.jpg",  BlogCreateDate = DateTime.Now, BlogStatus = true, CategoryID = 1 });
+            blogs.Add(new EntityLayer.Concrete.Blog() { BlogTitle="Wild rift", BlogImage = "/web/images/7.jpg",  BlogCreateDate = DateTime.Now, BlogStatus = true, CategoryID = 4 });
+            //var values = bm.GetList(); data bazaya elave edennen sora ustu silif buynan davam edirsen
+            return View(blogs);
         }
     }
 }
---
+++ BusinessLayer/Concrete/BlogManeger.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class BlogManeger : IBlogService
+    {
+        IBlogDal blogDal;
+
+        public BlogManeger(IBlogDal blogDal)
+        {
+            this.blogDal = blogDal;
+        }
+
+        public void AddBlog(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBlog(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Blog GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Blog> GetList()
+        {
+            return blogDal.GetListAll();
+        }
+
+        public void UpdateBlog(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
