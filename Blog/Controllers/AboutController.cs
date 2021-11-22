42518ac02edc7c67c0cb39667f4fa74d43a99047
SamurAgani <76554296+SamurAgani@users.noreply.github.com>
2021-11-22 16:24:06 +0400

    About

---
+++ Blog/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManeger abm = new AboutManeger(new EfAboutRepository());
+        public IActionResult Invoke()
+        {
+            return View();
+        }
+        public PartialViewResult SocialMediaAbout()
+        {
+            var values = abm.GetList();
+            return PartialView(values);
+        }
+    }
+}
+
--- Blog/Controllers/BlogController.cs
+++ Blog/Controllers/BlogController.cs
@@ -27,5 +27,6 @@ namespace Blog.Controllers
             return View(values);
         }
     }
+
 }
 
