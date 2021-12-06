f363abd0f94aabb73140fc5cb276c36dde13531a
SamurAgani <76554296+SamurAgani@users.noreply.github.com>
2021-12-06 11:46:58 +0400

    Messages_added

---
+++ BusinessLayer/Concrete/MessageManager.cs
@@ -0,0 +1,53 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class MessageManager : IMessageService
+    {
+        IMessageDal messageDal;
+
+        public MessageManager(IMessageDal messageDal)
+        {
+            this.messageDal = messageDal;
+        }
+
+        public void Add(Message t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Message t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Message GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public List<Message> GetList()
+        {
+            return messageDal.GetListAll();
+        }
+
+        public List<Message> GetInboxListByWriter(string p)
+        {
+            return messageDal.GetListAll(x=>x.Receiver == p);
+        }
+
+        public void Update(Message t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- DataAccessLayer/Migrations/20211206072528_mi_add_Messages.Designer.cs
+++ DataAccessLayer/Migrations/20211206072528_mi_add_Messages.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20211206072528_mi_add_Messages")]
+    partial class mi_add_Messages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
