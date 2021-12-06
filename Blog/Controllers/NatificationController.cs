91eb9587670775e6839255b21872a0d388db0ab6
SamurAgani <76554296+SamurAgani@users.noreply.github.com>
2021-12-06 15:48:21 +0400

    notification_added

--- Blog/Controllers/NatificationController.cs
+++ Blog/Controllers/NatificationController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers
 {
     public class NatificationController : Controller
-    { 
+    {
+        NatificationManager nm = new NatificationManager(new EfNatificationRrepository());
         public IActionResult Index()
         {
             return View();
+        }
+        [AllowAnonymous]
+        public IActionResult AllNatifation()
+        {
+            var values = nm.GetList();
+            return View(values);
         }
     }
 }
--- Blog/ViewComponents/Writer/WriterMessageNatification.cs
+++ Blog/ViewComponents/Writer/WriterMessageNatification.cs
@@ -1,11 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 namespace Blog.ViewComponents
 {
     public class WriterMessageNatification : ViewComponent
     {
+        MessageManager mm = new MessageManager(new EfMessageRepository());
         public IViewComponentResult Invoke()
         {
-            return View();
+            string p = "test@gmail.com";
+            var values = mm.GetInboxListByWriter(p);
+            return View(values);
         }
     }
 }
