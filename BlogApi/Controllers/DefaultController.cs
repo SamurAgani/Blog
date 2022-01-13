8186173c565e5fbb12ef0b384c3487ac9031c657
SamurAgani <76554296+SamurAgani@users.noreply.github.com>
2022-01-13 16:42:45 +0400

    ApiStart

---
+++ BlogApi/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using BlogApi.DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult EmployeeList()
+        {
+            using var c = new Context();
+            var values = c.Employee.ToList();
+            return Ok(values);
+        }
+        
+    }
+}
---
+++ BlogApi/DataAccessLayer/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApi.DataAccessLayer
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-28DRIHF\\SQLEXPRESS;Trusted_Connection=True;database=CoreBlogApiDb;" +
+                "integrated security=true;");
+
+
+        }
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
